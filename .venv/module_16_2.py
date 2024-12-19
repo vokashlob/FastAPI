3a3f160a1fb39f644597b18dc340f7db891bf916
vokashlob <58624355+vokashlob@users.noreply.github.com>
2024-12-19 20:57:00 +0300

    first commit

--- .venv/module_16_2.py
+++ .venv/module_16_2.py
@@ -12,13 +12,22 @@ async def admin():
     return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def get_user(user_id: Annotated[int, Path(gt=0,
-                                                ge=100,
-                                                title="User ID",
-                                                description="Enter user ID",
-                                                example=1)]):
+async def get_user(user_id: int = Path(ge=1,
+                                       le=100,
+                                       title="user ID",
+                                       description="Enter user ID",
+                                       example='1')):
     return f'Вы вошли как пользователь №{user_id}'
 
-@app.get('/user')
-async def user_info(username, age):
+@app.get('/user/{username}/{age}')
+async def user_info(username: Annotated[str, Path(min_length=5,
+                                                  max_length=20,
+                                                  title='Username',
+                                                  description='Enter username',
+                                                  example='UrbanUser')],
+                    age: Annotated[int, Path(ge=18,
+                                             le=120,
+                                             title='Age',
+                                             description='Enter age',
+                                             example='24')]):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
